134dcafef9bf805561a5ffc611dc5568ee1a063d
Rio Chandra <riochandra4@gmail.com>
2021-08-02 00:09:14 +0700

    feat: add page contactMe

---
+++ components/ContactMe.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+      <h3>Contact Me</h3>
+      <div class="mb-2">
+        You can follow my activity by join to my social media :
+      </div>
+      <div class="mb-2">
+        <a
+          href="https://www.facebook.com/riochndr726/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h5>
+            <v-icon>mdi-facebook</v-icon>
+            Facebook
+          </h5>
+        </a>
+      </div>
+      <div class="mb-2">
+        <a
+          href="https://www.instagram.com/riochndr/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h5>
+            <v-icon>mdi-instagram</v-icon>
+            Instagram
+          </h5>
+        </a>
+      </div>
+      <div class="mb-2">
+        <a href="mailto:riochandra4@gmail.com">
+          <h5>
+            <v-icon>mdi-email</v-icon>
+            Or directly Email Me
+          </h5>
+        </a>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
