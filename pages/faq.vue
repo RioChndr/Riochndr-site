069142aaf6a55bae1f4a4d5a7d248d5029f7474a
Rio Chandra <riochandra4@gmail.com>
2021-08-02 00:09:26 +0700

    feat: add page faq

---
+++ pages/faq.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2 class="mb-6">FAQ (Frequently Asked Questions)</h2>
+    <div>
+      <v-expansion-panels>
+        <v-expansion-panel v-for="(item, i) in faqs" :key="i">
+          <v-expansion-panel-header>
+            <strong>
+              {{ item.question }}
+            </strong>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            {{ item.answer }}
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      faqs: [
+        {
+          question: 'What framework always you use ?',
+          answer: 'Front end using Vue js and Nuxt',
+        },
+        {
+          question: 'Can you hack my ex facebook account ?',
+          answer: "Really? 😑 No !!. I'm not author facebook",
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
